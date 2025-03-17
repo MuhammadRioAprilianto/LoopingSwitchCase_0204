dd2dfe0931211dbf20c6792997dff2030c59daff
MuhammadRioAprilianto <rma994636@gmail.com>
2025-03-17 16:23:14 +0700

    membuat program switch case

---
+++ contoh_switchCase.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+double hitungIpk(double total, int sks)
+{
+    return total / sks;
+}
+
+string predikat (double ipk){
+    if (ipk < 10){
+        return "memuaskan";
+    }
+    else{
+        return "cumlaude";
+    }
+}
+
+
+int main()
+{
+    char pilihan;
+    int sks;
+    double total;
+    do
+    {
+        cout << "Pilih Menu" << endl;
+        cout << "1. Cek nilai IPK" << endl;
+        cout << "2. Cek predikat kelulusan" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "masukkan pilihan (1\2\3) = " << endl;
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case '1':
+            
+            cout << "masukkan total nilai = ";
+            cin >> total;
+            cout << "masukkan jumlah sks = ";
+            cin >> sks;
+
+            cout << "Nilai Ipk = " << hitungIpk(total, sks);
+            break;
+        case '2':
+            cout << "Predikat anda adalah " << predikat(hitungIpk(total,sks)) << endl;
+            break;
+        case '3':
+            break;
+        default:
+            cout << "pilihan anda salah " << endl;
+            break;
+        }
+    }while (pilihan != 3);
+    
+}
