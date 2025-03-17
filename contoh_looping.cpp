a6265aa2aa7b5ea48b708c2bd002529b400c1ab0
MuhammadRioAprilianto <rma994636@gmail.com>
2025-03-17 15:25:17 +0700

    membuat program perulangan FOR pencacah naik

--- contoh_looping.cpp
+++ contoh_looping.cpp
@@ -8,4 +8,14 @@ int main()
     int n;
     int perulanganWhile = 0;
     int perulanganDo = 0;
+
+    //perulangan dengan FOR
+
+    cout << "perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang " << endl; 
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
 }
